9774870d48ec8e690230d31ac7894d9da84596e1
Sagillaire <Jasantise@outlook.es>
2023-09-03 23:45:21 -0500

    Feature: Add functionality to users services => findOne, findAll, update

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
+import { User, UserSchema } from 'src/auth/schema/auth.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { HttpStatus, Injectable, HttpException } from '@nestjs/common';
+import { User, UserDocument } from 'src/auth/schema/auth.schema';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async findAll() {
+    try {
+      const findAll = await this.userModel.find().select('-password');
+      return { count: findAll.length, results: findAll };
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findOne(id: string) {
+    try {
+      const findOne = await this.userModel.findById(id);
+      if (!findOne)
+        throw new HttpException('USER_NOT_FOUND', HttpStatus.NOT_FOUND);
+      return findOne;
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const { password } = updateUserDto;
+    try {
+      const findOne = await this.userModel.findById(id);
+      if (!findOne)
+        throw new HttpException('USER_NOT_FOUND', HttpStatus.NOT_FOUND);
+      const passwordHash = await hash(password, 8);
+      updateUserDto = { ...updateUserDto, password: passwordHash };
+      const update = await this.userModel.updateOne({ _id: id }, updateUserDto);
+      return update;
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
